f2ff4174352f316fdf6705b4b97e79aa5e887179
chatelp@gmail.com <y2ZFcbG8>
2015-06-06 16:19:20 +0200

    Nouvelle icone

--- mobile/src/main/java/pm/chatel/senbazuru/service/FetcherService.java
+++ mobile/src/main/java/pm/chatel/senbazuru/service/FetcherService.java
@@ -233,7 +233,7 @@ public class FetcherService extends IntentService {
                         Notification.Builder notifBuilder = new Notification.Builder(MainApplication.getContext()) //
                                 .setContentIntent(contentIntent) //
                                 .setSmallIcon(R.drawable.ic_statusbar_rss) //
-                                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher)) //
+                                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.android_icon)) //
                                 .setTicker(text) //
                                 .setWhen(System.currentTimeMillis()) //
                                 .setAutoCancel(true) //
